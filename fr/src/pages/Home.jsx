4754d285f4f27d5876e7a45022d7f5f6fff48c1e
DeepanB2005 <deepanb2005@gmail.com>
2025-05-26 00:41:33 +0530

    directory structure update

---
+++ fr/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import navbar from "../components/navbar";
+function Home() {
+    return (
+        <div className="bg-gray-100 min-h-screen">
+            {navbar()}
+        </div>
+    );
+}
+export default Home;
