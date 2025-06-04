001f2acfe1786105c99cf1624c67ab309776d9b0
DeepanB2005 <deepanb2005@gmail.com>
2025-06-04 17:44:21 +0530

    cleanup folders

--- bc/server.js
+++ bc/server.js
@@ -1,3 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express =require('express');
+const app =express();
+const port = 3000;
+app.listen(port,(e)=>{
+    if(e) {
+        console.error("Error starting server:", e);
+        return;
+    }else{
+    console.log(`Server is running on http://localhost:${port}`);
+    console.log("vanakkam da mapla")
+    }
+})
