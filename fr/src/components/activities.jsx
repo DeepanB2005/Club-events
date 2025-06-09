1a2a56b0ec6b16767433ac50c4f57d8c197708be
DEEPAN . B <deepanb2005@gmail.com>
2025-06-09 20:45:39 +0530

    activities styles

--- fr/src/components/activities.jsx
+++ fr/src/components/activities.jsx
@@ -1,6 +1,6 @@
 function Activities() {
   return (
-    <div className="container mx-auto px-4 py-8 bg-red-400 mt-7 h-screen">
+    <div className="container mx-auto px-4 py-8 bg-red-400 mt-7 w-[1300px] h-screen rounded-lg">
       <h1 className="text-4xl font-bold mb-6">Activities</h1>
       <h2>Activities</h2>
       <p>Here you can find various activities to engage with.</p>
--- fr/src/pages/Home.jsx
+++ fr/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Hero from "../components/hero";
 import Activities from "../components/activities";  
 function Home() {
     return (
-        <div className="text-white font-ft bg-white dark:bg-blue-950 ">
+        <div className="text-white font-ft bg-gradient-to-r from-blue-200 to-purple-100 dark:bg-gradient-to-t dark:from-[#0f051022] dark:to-[#7b0acb]   dark:bg-blue-950 ">
             <Navbar/>
             <Hero/>
             <Activities/>
