eaf93bb7a7434365def73e2c6c6407b362c780bb
DEEPAN . B <deepanb2005@gmail.com>
2025-06-09 20:49:46 +0530

    activities styles

--- fr/src/components/activities.jsx
+++ fr/src/components/activities.jsx
@@ -1,6 +1,6 @@
 function Activities() {
   return (
-    <div className="container mx-auto px-4 py-8 bg-red-400 mt-7 w-[1300px] h-screen rounded-lg">
+    <div className="container mx-auto px-4 py-8 bg-pink-100  mt-7 w-[1300px] h-screen rounded-lg">
       <h1 className="text-4xl font-bold mb-6">Activities</h1>
       <h2>Activities</h2>
       <p>Here you can find various activities to engage with.</p>
