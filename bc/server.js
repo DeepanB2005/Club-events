a794bfb9d12fc4c8dee76e6c50edd0d402b4cb01
DeepanB2005 <deepanb2005@gmail.com>
2025-06-16 15:40:59 +0530

    backend directory structure

--- bc/server.js
+++ bc/server.js
@@ -1,22 +1,14 @@
 const express =require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
 const app =express();
 const port = 3000;
 app.listen(port,(e)=>{
-    if(e) {
+    if(e) 
+    {
         console.error("Error starting server:", e);
-        return;
-    }else{
+    }
+    else
+    {
     console.log(`Server is running on http://localhost:${port}`);
     console.log("vanakkam da mapla");
     }
 })
-
-
-
-
-const mongoURI = process.env.MONGODB_URI;
-mongoose.connect(mongoURI)
-  .then(() => console.log('Connected to MongoDB Atlas'))
-  .catch(err => console.error('Connection error:', err));
--- zzz.java
+++ zzz.java
@@ -1,7 +1,19 @@
-class zzz
+class fizz
 {
     public static void main(String[] args) {
-        int a = 03;
-        System.out.println(a);
+        for(int i=1;i<=15;i++)
+        {
+            if(i%3==0)
+            {
+                System.out.println("Fizz");
+            }
+            else if(i%5==0)
+            {
+                System.out.println("Buzz");
+            }
+            else{
+                System.out.println(i);
+            }
+        }
     }
 }
